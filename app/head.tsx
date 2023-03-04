f7bc63a2c38e6a08307a29eb2839ced250b162ed
milosdjurica <milosdjurica13@gmail.com>
2023-03-04 08:51:22 +0100

    head change

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,13 @@
 export default function Head() {
   return (
     <>
-      <title>Linktree</title>
-      <meta name="description" content="Next.js + Tailwind CSS" />
+      <title>Milos Djurica | Linktree Clone</title>
+      <meta
+        name="description"
+        content="Linktree clone | Milos Djurica - full stack software engineer"
+      />
       <link rel="icon" href="/favicon.ico" />
+      <meta name="author" content="Milos Djurica" />
       <meta name="viewport" content="width=device-width, initial-scale-1" />
     </>
   );
